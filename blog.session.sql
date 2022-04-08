84461d79da2c6c3443b7314c6a7b97beda755218
scheikh <abdulrahman404@hotmail.com>
2022-04-09 00:37:48 +0200

    first commit, lanch Blog (course project)

---
+++ blog.session.sql
@@ -0,0 +1,4 @@
+CREATE TABLE posts (
+title VARCHAR(30) NOT NULL,
+content TEXT(500) NOT NULL
+)
---
+++ database.php
@@ -0,0 +1,20 @@
+<?php
+$pdo = new PDO(
+  'mysql:host=localhost;dbname=blog',
+  'root',
+  ''
+);
+
+function fetch_posts()
+{
+   global $pdo;
+   return $pdo->query("SELECT * FROM `posts`");
+}
+
+function fetch_post($title)
+{
+  global $pdo;
+  $q = $pdo->query("SELECT * FROM `posts` WHERE title='{$title}'");
+  return $q->fetch();
+}
+?>
