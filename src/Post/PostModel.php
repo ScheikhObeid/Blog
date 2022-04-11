467bff5e71ca47f8eb1ab3772f0c961f3b42f256
scheikh <abdulrahman404@hotmail.com>
2022-04-11 19:40:46 +0200

    Adding ArrayAccess-Interface to the PostModel

--- src/Post/PostModel.php
+++ src/Post/PostModel.php
@@ -1,13 +1,27 @@
 <?php
 namespace App\Post;
 
-class PostModel {
+use ArrayAccess;
+
+class PostModel implements ArrayAccess {
+
     public $id;
     public $title;
     public $content;
 
-    public function getShortContent(){
-        
+    public function offsetExists($offset){
+        return isset($this->$offset);
+    }
+
+    public function offsetGet($offset){
+        return $this->$offset;
+    }
+
+    public function offsetSet($offset, $value){
+        $this->$offset = $value;
+    }
+    public function offsetUnset($offset){
+        unset($this->$offset);
     }
 
 }
