6dcabd2e56b543fc5cc00367b6e1ba1cc998ddbd
scheikh <abdulrahman404@hotmail.com>
2022-04-11 22:12:37 +0200

    Moving PostsRepository into an own container

--- database.php
+++ database.php
@@ -1,11 +1,4 @@
 <?php
-$pdo = new PDO(
-  'mysql:host=localhost;dbname=blog; charset=utf8',
-  'root',
-  ''
-);
-$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
 
 
 ?>
--- init.php
+++ init.php
@@ -2,5 +2,8 @@
 
 require __DIR__ .'./autoload.php';
 require __DIR__ .'./database.php';
+require __DIR__ .'./Core/Container.php';
+
+$container = new App\Core\Container();
 
 ?>
