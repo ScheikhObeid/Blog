3db9a59cbc2bc59e1cbef164ea0043ddd2725ed6
scheikh <abdulrahman404@hotmail.com>
2022-04-13 15:00:50 +0200

    Routing - Raouting optimization

--- public/index.php
+++ public/index.php
@@ -1,12 +1,30 @@
 <?php require ("../init.php");
-$pathInfo = $_SERVER["PATH_INFO"];
+$pathInfo = $_SERVER['PATH_INFO'];
 
+$routes = 
+[
+    '/index' => [
+        'controller' => 'postsController',
+        'method' => 'index',
+    ],
+    '/post' => [
+        'controller' => 'postsController',
+        'method' => 'post',
+    ]
+];
 
-if($pathInfo == "/index"){
-
-    $postsController = $container->make("postsController");
-    $postsController->index();
-}elseif($pathInfo == "/post"){
-    $postsController = $container->make("postsController");
-    $postsController->show();
+if(isset($routes['$pathInfo'])){
+$route = $routes['$pathInfo'];
+$controller = $container->make($route['controller']);
+$method = $route['method'];
+$controller->$method();
 }
+
+// if($pathInfo == "/index"){
+
+//     $postsController = $container->make("postsController");
+//     $postsController->index();
+// }elseif($pathInfo == "/post"){
+//     $postsController = $container->make("postsController");
+//     $postsController->show();
+// }
