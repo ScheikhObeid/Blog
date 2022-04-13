e86a5711ad05a6091d2cfded1f7856bfe61ad090
scheikh <abdulrahman404@hotmail.com>
2022-04-13 14:28:42 +0200

    Routing - Adding Raouting in index.php and changing pages links

--- pages/post.php
+++
@@ -1,3 +0,0 @@
-<?php include("../init.php");
-$postsController = $container->make("postsController");
-$postsController->show();
---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php require ("../init.php");
+$pathInfo = $_SERVER["PATH_INFO"];
+
+
+if($pathInfo == "/index"){
+
+    $postsController = $container->make("postsController");
+    $postsController->index();
+}elseif($pathInfo == "/post"){
+    $postsController = $container->make("postsController");
+    $postsController->show();
+}
